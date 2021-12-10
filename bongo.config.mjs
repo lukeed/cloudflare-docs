30ae770de29e749e380f8d31218a497d52ce2e38
Luke Edwards <luke.edwards05@gmail.com>
2021-12-10 11:40:39 -0800

    chore: bump bongo version

--- bongo.config.mjs
+++ bongo.config.mjs
@@ -1,9 +1,13 @@
 import { define } from 'bongo';
+import * as markdown from '@bongo/markdown';
 
 export default define({
 	$site: {
 		lang: 'en',
-		title: 'My Website',
-		url: 'https://example.com',
-	}
+		title: 'Cloudflare Developers',
+		url: 'https://developers.cloudflare.com',
+	},
+	plugins: [
+		markdown,
+	]
 });
