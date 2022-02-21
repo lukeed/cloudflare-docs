2944541da5ca2b4fff2b0ab0fa10854afb234a2f
Adam Janiš <eidam@cloudflare.com>
2022-02-21 12:59:31 +0000

    init coveo mvp

---
+++ assets/search-coveo.ts
@@ -0,0 +1,72 @@
+let $ = document.querySelector.bind(document);
+let coveo = window.Coveo
+
+function loadCustomSearchBox() {
+  let element = $('#DocsSearch--input') || $('#SiteSearch--input');  
+  const CustomSearchbox = (function(_super) {
+    __extends(CustomSearchbox, coveo.Component);
+    function CustomSearchbox(element, options, bindings) {
+      _super.call(this, element, CustomSearchbox.ID, bindings);
+      this.type = 'CustomSearchBox';
+      coveo.Component.bindComponentToElement(element, this);
+      this.element = element;
+      this.options = coveo.ComponentOptions.initComponentOptions(element, CustomSearchbox, options);
+      this.bindings = bindings;
+      this.element.addEventListener('keyup', (e) => this.handleKeyUp(e));
+    }
+    CustomSearchbox.prototype.handleKeyUp = function(e) {
+      if (this.options.searchAsYouType) {
+        this.executeNewQuery();
+      } else if (e.key == 'Enter') {
+        this.executeNewQuery();
+      }
+    }
+    CustomSearchbox.prototype.executeNewQuery = function() {
+      this.bindings.queryStateModel.set('q', this.element.value);
+      //this.bindings.usageAnalytics.logSearchEvent({
+      //  name: 'submitSearchbox',
+      //  type: 'CustomSearchbox'
+      //});
+      this.bindings.queryController.executeQuery();
+    }
+    CustomSearchbox.options = {
+      searchAsYouType: coveo.ComponentOptions.buildBooleanOption({ defaultValue: false })
+    }
+    CustomSearchbox.ID = "CustomSearchBox";
+    coveo.Initialization.registerAutoCreateComponent(CustomSearchbox);
+  })(coveo.Component);
+
+  coveo.SearchEndpoint.configureCloudEndpoint('cloudflareproduction7iy98g48', 'xx37b7bfe4-2b6c-4720-a42a-11e9e88f5315');
+  coveo.initSearchbox($('.CoveoSearchInterface'), "/search")
+
+  addEventListener('keydown', ev => {
+    if (ev.target === element) return;
+
+    let key = ev.which;
+
+    // is '/' or SHIFT+'s'
+    if (key === 191 || (ev.shiftKey && key === 83)) {
+      ev.preventDefault();
+      window.scrollTo(0, 0);
+      element.focus();
+    }
+  });
+}
+
+function loadSearchResults() {
+  // The following line shows you how you could configure an endpoint against which to perform your search.
+  coveo.SearchEndpoint.configureCloudEndpoint('cloudflarenonproduction1bqz82hwh', 'xx59d99d0b-6982-495a-b1b9-ec1947377046');
+
+  // Initialize the framework by targeting the root in the interface.
+  // It does not have to be the document body.
+  coveo.init(document.getElementById('searchresults'));
+}
+
+// init
+(function load() {
+  if (window.location.pathname.startsWith("/search")) {
+    loadSearchResults();
+  } else {
+    loadCustomSearchBox();
+  }
+})();
